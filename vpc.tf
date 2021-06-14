0f4cdd5aa5173d97ac82f28da622b75427d36f33
haritha <dlatheef@gmail.com>
2021-06-14 17:08:36 +0530

    vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "default" {
+  cidr_block = "40.0.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "DEVOPS"
+    Owner = "Devopstem"
+    environment = "Devlopment"
+    Deployedby = "DevopsTeam" 
+}
+}
